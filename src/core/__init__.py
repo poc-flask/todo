2ccbd1cc769b2a895dfd6a0440fdaa21d544da4e
tranhnb <tranhnb@gmail.com>
2018-10-14 17:47:58 -0400

    Load spatialite extension for sqlite3 for local development.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -6,6 +6,7 @@ from sqlalchemy.event import listen
 from .api import CoreApi
 from .models import db
 from .create_app import create_app
+from .spatialite_extension import load as load_spatialite_extension
 
 app = create_app(__name__)
 
@@ -16,16 +17,7 @@ api = CoreApi(app)
 # Sync up database
 db.app = app
 db.init_app(app)
-print('debug')
-print(type(db.engine))
-
-def load_spatialite(dbapi_conn, connection_record):
-    print('ahihi')
-    dbapi_conn.enable_load_extension(True)
-    dbapi_conn.load_extension('/usr/local/lib/mod_spatialite.dylib')
-
-listen(db.engine, 'connect', load_spatialite)
-
+load_spatialite_extension(db.engine)
 
 bcrypt = Bcrypt()
 bcrypt.init_app(app)
@@ -33,5 +25,4 @@ bcrypt.init_app(app)
 # Setup the Flask-JWT-Extended extension
 jwt = JWTManager(app)
 
-migrate = Migrate(app, db) # this
-print(type(migrate))
+migrate = Migrate(app, db)
